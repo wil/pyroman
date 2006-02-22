8de1445850e34a339eb56db31157b4afc57f0157
erich <erich@7e9cc6e5-af0c-0410-8aaf-91426f4e657c>
2006-02-22 15:49:23 +0000

    better description
    git-svn-id: svn://svn.debian.org/pyroman/trunk@16 7e9cc6e5-af0c-0410-8aaf-91426f4e657c

--- examples/00_flush.py
+++ examples/00_flush.py
@@ -1,15 +1,16 @@
 """
-These are basic rules to reset the firewall to an "empty" state,
-by emptying any chain and removing them
+These are the iptables builtin chains.
+In order to use the builtins in Pyroman, we need to define them, too.
+Note that we set the default actions here, too.
 """
-add_chain("INPUT", default="DROP")
-add_chain("OUTPUT", default="DROP")
-add_chain("FORWARD", default="DROP")
-add_chain("OUTPUT", id="natOUT", default="ACCEPT", table="nat")
-add_chain("PREROUTING", id="natPRE", default="ACCEPT", table="nat")
+add_chain("INPUT",                     default="DROP")
+add_chain("OUTPUT",                    default="DROP")
+add_chain("FORWARD",                   default="DROP")
+add_chain("OUTPUT",      id="natOUT",  default="ACCEPT", table="nat")
+add_chain("PREROUTING",  id="natPRE",  default="ACCEPT", table="nat")
 add_chain("POSTROUTING", id="natPOST", default="ACCEPT", table="nat")
-add_chain("INPUT", id="manIN", default="ACCEPT", table="mangle")
-add_chain("OUTPUT", id="manOUT", default="ACCEPT", table="mangle")
-add_chain("FORWARD", id="manFWD", default="ACCEPT", table="mangle")
-add_chain("PREROUTING", id="manPRE", default="ACCEPT", table="mangle")
+add_chain("INPUT",       id="manIN",   default="ACCEPT", table="mangle")
+add_chain("OUTPUT",      id="manOUT",  default="ACCEPT", table="mangle")
+add_chain("FORWARD",     id="manFWD",  default="ACCEPT", table="mangle")
+add_chain("PREROUTING",  id="manPRE",  default="ACCEPT", table="mangle")
 add_chain("POSTROUTING", id="manPOST", default="ACCEPT", table="mangle")
