28b71011c6bc02c1985977840e924d8ff0db5139
erich <erich@7e9cc6e5-af0c-0410-8aaf-91426f4e657c>
2007-03-01 13:44:44 +0000

    add alias for unprivileged ports
    git-svn-id: svn://svn.debian.org/pyroman/trunk@41 7e9cc6e5-af0c-0410-8aaf-91426f4e657c

--- examples/base/20_services.py
+++ examples/base/20_services.py
@@ -49,3 +49,6 @@ add_service("win2t", dports="137:139/tcp 445/tcp")
 add_service("win1u", sports="137:139/udp 445/udp")
 add_service("win2u", dports="137:139/udp 445/udp")
 add_service("win", include="win1t win2t win1u win2u")
+
+# unprivileged ports
+add_service("unprivileged", dports="1024:65535/tcp 1024:65535/udp")
