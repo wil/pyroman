2f17a7d5399bde888ac951131f11b4e4efa6ddf9
erich <erich@7e9cc6e5-af0c-0410-8aaf-91426f4e657c>
2008-06-05 16:29:31 +0000

    remove bad configuration 'example files' from base - they belong into example1, personal-firewall etc
    git-svn-id: svn://svn.debian.org/pyroman/trunk@72 7e9cc6e5-af0c-0410-8aaf-91426f4e657c

--- examples/base/10_interfaces.py
+++ examples/base/10_interfaces.py
@@ -1,6 +1,5 @@
 """
 We create our interface groups here.
 """
-add_interface("int", "eth0 ppp+")
-add_interface("dmz", "eth1")
-add_interface("ext", "eth2")
+# setup an interface/group alias here, for example:
+# add_interface("int", "eth0 ppp+")
--- examples/base/25_networks.py
+++ examples/base/25_networks.py
@@ -2,22 +2,9 @@
 Define the networks available here, or more precisely "hostgroups".
 Many of your policy rules will probably target whole subnets.
 """
-# First is the internal network, we're using a /24 network here only
-# and it's connected to our "internal" interface
-add_host(
-	name="INT",
-	ip="10.0.0.0/24",
-	iface="int"
-)
-# Our DMZ network uses another /24 network
-add_host(
-	name="DMZ",
-	ip="10.0.1.0/24",
-	iface="dmz"
-)
-# Any other host, connected to our "external" interface
-add_host(
-	name="ANY",
-	ip="0.0.0.0/0",
-	iface="ext"
-)
+# setup host groups here, for example like this:
+#add_host(
+#	name="INT",
+#	ip="10.0.0.0/24",
+#	iface="int"
+#)
